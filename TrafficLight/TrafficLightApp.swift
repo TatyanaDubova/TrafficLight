78332539ec924288426f8b021b8eb1fc3ba00f1a
tatanadubova <tatyana.dubova.1986@gmail.com>
2023-12-12 12:33:10 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Татьяна Дубова on 12.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
